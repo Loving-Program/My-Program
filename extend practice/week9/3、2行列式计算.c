424d0bc52f977814a230a4f754dae9ad5dadba10
苏晓甜 <15931829094@163.com>
2017-11-24 18:00:06 +0800

    add

---
+++ extend practice/week9/3、2行列式计算.c
@@ -0,0 +1,24 @@
+//2*2行列式计算 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h> //用到了time函数 
+int main()
+{ 
+	int m,n,det,a[2][2]; 
+	srand((unsigned) time(NULL)); //用时间做种，每次产生随机数不一样
+	for(m=0;m<=1;m++)
+		for(n=0;n<=2;n++)
+			a[m][n]=rand()%100; //产生0-1000的随机数
+	printf("array a:\n");
+	for(m=0;m<=1;m++)
+	{
+		for(n=0;n<=1;n++)
+		{	
+			printf("%d\t",a[m][n]);
+			if(n==1)
+				printf("\n");
+		}
+	}
+	det=a[0][0]*a[1][1]-a[0][1]*a[1][0];
+	printf("sum of determinant=%d",det);
+}
