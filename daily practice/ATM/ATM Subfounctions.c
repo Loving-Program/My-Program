9c695d19765ce18f0a6745ff566cfcc64e758027
苏晓甜 <15931829094@163.com>
2017-12-11 21:44:08 +0800

    update ATM

--- daily practice/ATM/ATM Subfounctions.c
+++ daily practice/ATM/ATM Subfounctions.c
@@ -1,21 +1,30 @@
 /* Note:Your choice is C IDE */
 #include "stdio.h"
+void Checkpassword();
+void service();
+void deposit();
+void withdraw();
+void sum();
+void ChangePassword();
 void main()
 {
-    int account,pwd,i;
+    int account;
+    printf("－－－欢迎使用ATM机－－－\n");
     printf("请插卡：\n");
     scanf("%d",&account);
-    if(account==111)
+    Checkpassword(account);
+}
+void Checkpassword(int account)
+{
+	int pwd,i;
+	if(account==111)
     {
     	printf("请输入您的密码后按确认键:\n");
     	for(i=1;i<=3;i++)
     	{
 			scanf("%d",&pwd);
     		if(pwd==666)
-    		{	
-				void service();
-    			service();
-    		}
+				service();
     		else
 	    		if(i!=3)
 	    			printf("您的密码输入错误,请重新输入，您还有%d次机会。\n",3-i);
@@ -28,19 +37,19 @@ void main()
 }
 void service()
 {
-	void deposit();
-	void withdraw();
-	void sum();
-	int x;
-	printf("1.存款\n2.取款\n3.查询余额\n");
+	int x; 
+	printf("1.存款      2.取款\n");
+	printf("3.查询余额  4.修改密码\n");
+	printf("5.转账      6.退出系统\n");
+	printf("请选择服务类型：\n");
 	scanf("%d",&x);
-	if(x==1)
-		deposit();
-	if(x==2)
-		withdraw();
-	if(x==3)
-		sum();
-		
+	switch(x)
+	{
+		case 1:deposit();break;
+		case 2:withdraw();break;
+		case 3:sum();break;
+		case 4:ChangePassword();break;
+	}		
 }
 void deposit()
 {
@@ -48,7 +57,9 @@ void deposit()
 	printf("请输入您的存款金额:\n");
 	scanf("%d",&m1);
 	printf("请放入您的钞票，本机只接收100元纸币:\n");
-	printf("您放入钞票明细如下:\n请选择是否继续放钞:1、继续放钞\t2、结束放钞\n");
+	printf("正在验钞，请稍后\n");
+	printf("您放入钞票明细如下:\n");
+	printf("请选择是否继续放钞:1、继续放钞\t2、结束放钞\n");
 	scanf("%d",&a);
 	if(a==1)
 		printf("请放入您的钞票，本机只接收100元纸币:\n");
@@ -62,14 +73,52 @@ void deposit()
 void withdraw()
 {
 	int m2;
-	printf("请输入您的取款金额:\n单笔最大取款金额为5,000.00元\n");
+	printf("请输入您的取款金额（整百）:\n");
+	printf("单笔最大取款金额为5,000元\n");
 	scanf("%d",&m2);
-
+	while (m2!=0||m2==0)
+	{
+        printf("取款金额有误，请输入整百！\n");
+        printf("请重新输入您的取款金额（整百）:\n");
+        scanf("%d",&m2);
+//判断余额是否充足 
+	}
 
 }
 void sum()
 {
 	printf("您的账户余额为：\n");
 	printf("可用余额为：\n");
-	
  } 
+void ChangePassword()
+{
+	int pwd=666,password,i; 
+	int newpwd1,newpwd2;
+	printf("请输入银行卡原密码：\n");
+	for(i=1;i<=3;i++)
+   	{
+		scanf("%d",&pwd);
+    	if(pwd==666)
+	    	break;
+    	else
+	    	if(i!=3)
+	    		printf("您的密码输入错误,请重新输入，您还有%d次机会。\n",3-i);
+	   		else
+	    		printf("您已经错误三次，吞卡，联系发卡行。");
+   	}	
+   	printf("请输入新密码：");
+	scanf("%d",&newpwd1);
+	printf("请再次输入新密码：");
+	scanf("%d",&newpwd2);
+	while(newpwd1!=newpwd2)
+	{
+		printf("两次密码输入不一致，请重新输入\n");
+		printf("请输入新密码：");
+		scanf("%d",&newpwd1);
+		printf("请再次输入新密码：");
+		scanf("%d",&newpwd2);
+	}
+	printf("密码修改成功，请重新登录!\n");
+	Checkpassword();
+	
+}
